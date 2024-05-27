8a5e633931b5362ae079fe5010b55ae4703f5304
brunworks <brunworks@outlook.com>
2024-05-27 11:34:32 -0300

    Implementação do MFE2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AppComponent } from './app.component';
 
-const routes: Routes = [];
+const MFE2_ROUTES: Routes = [
+  {
+    path: '',
+    component: AppComponent,
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forChild(MFE2_ROUTES)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
